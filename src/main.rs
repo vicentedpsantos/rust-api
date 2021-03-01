09c35deb73c4ffe9361fbdf991e2fcb2fa263bb3
Vicente <vicentedpsantos@gmail.com>
2021-03-01 09:48:23 -0300

    Create mocked handlers

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,47 @@ extern crate rocket;
 #[macro_use]
 extern crate rocket_contrib;
 
+use rocket::response::status;
 use rocket_contrib::json::JsonValue;
 
-#[get("/")]
-fn hello() -> JsonValue {
-    json!("Hello, world")
+#[get("/rustaceans")]
+fn get_rustaceans() -> JsonValue {
+    json!([{ "id": 1, "name": "Vicente Santos" }, { "id": 2, "name": "Tamires Quito" }])
+}
+
+#[get("/rustaceans/<id>")]
+fn view_rustacean(id: i32) -> JsonValue {
+    json!({ "id": id, "name": "Vicente Santos" })
+}
+
+#[post("/rustaceans", format = "json")]
+fn create_rustacean() -> JsonValue {
+    json!({ "id": 3, "name": "Vicente Santos" })
+}
+
+#[put("/rustaceans/<id>", format = "json")]
+fn update_rustacean(id: i32) -> JsonValue {
+    json!({ "id": id, "name": "Vicente Santos" })
+}
+
+#[delete("/rustaceans/<_id>")]
+fn delete_rustacean(_id: i32) -> status::NoContent {
+    status::NoContent
 }
 
 #[rocket::main]
 async fn main() {
-    let _ = rocket::ignite().mount("/", routes![hello]).launch().await;
+    let _ = rocket::ignite()
+        .mount(
+            "/",
+            routes![
+                get_rustaceans,
+                view_rustacean,
+                create_rustacean,
+                update_rustacean,
+                delete_rustacean
+            ],
+        )
+        .launch()
+        .await;
 }
